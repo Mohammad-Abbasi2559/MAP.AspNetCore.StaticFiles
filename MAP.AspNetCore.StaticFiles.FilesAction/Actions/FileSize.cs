bbb4a0917eafb5c2d5e5c463a03e653bf888949b
Mohammad Abbasi <133187637+Mohammad-Abbasi2559@users.noreply.github.com>
2023-05-17 01:18:28 -0700

    add-upload-files

---
+++ MAP.AspNetCore.StaticFiles.FilesAction/Actions/FileSize.cs
@@ -0,0 +1,31 @@
+﻿using ByteSizeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAP.AspNetCore.StaticFiles.FilesAction.Actions
+{
+    internal static class FileSize
+    {
+        public static string Size(long size)
+        {
+            var fileSizeBit = ByteSize.FromBytes(Convert.ToInt32(size));
+
+            if (1024 > fileSizeBit.KiloBytes)
+            {
+                return  Convert.ToString(fileSizeBit.KiloBytes);
+            }
+            else if (1024 > fileSizeBit.MegaBytes)
+            {
+                return Convert.ToString(fileSizeBit.MegaBytes);
+            }
+            else
+            {
+                return Convert.ToString(fileSizeBit.GigaBytes);
+            }
+        }
+    }
+}
---
+++ MAP.AspNetCore.StaticFiles.FilesAction/Models/RootFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAP.AspNetCore.StaticFiles.FilesAction.Models
+{
+    public class RootFile
+    {
+        public int fileId { get; set; }
+        public string? fileName { get; set; }
+        public string? fileAddress { get; set; }
+        public string? alt { get; set; }
+        public string? title { get; set; }
+        public string? fileType { get; set; }
+        public DateTime date { get; set; }
+        public string? sourceRedirect { get; set; }
+        public string? fileSize { get; set; }
+    }
+}
